ca0ac718b6d4f9ef3edc0c9aa969319fcfe8866f
djcproducts <72267157+djcproducts@users.noreply.github.com>
2020-11-25 17:48:32 -0600

    Update overlay.dart

--- lib/src/overlay.dart
+++ lib/src/overlay.dart
@@ -100,7 +100,7 @@ class _MySelectionList extends StatelessWidget {
     return Material(
       type: MaterialType.transparency,
       child: Container(
-        height: MediaQuery.of(context).size.height / 2,
+        height: MediaQuery.of(context).size.height,
         child: NotificationListener<ScrollNotification>(
           onNotification: (scrollNotification) {
             if (scrollNotification is ScrollEndNotification) {
